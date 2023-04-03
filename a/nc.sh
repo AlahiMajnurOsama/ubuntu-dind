7ed091de98c7c3ea8c4be738cfaccfe2dc6d7d73
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-04-03 00:33:02 -0700

    Delete a directory

--- a/nc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download netcat
-sudo apt-get update
-sudo apt-get install netcat -y
-
-# Connect to IP address and port
-nc 3.111.20.191 7262
--- a/setup.sh
+++
@@ -1,9 +0,0 @@
-apt-get install sudo -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo chmod +x xyz/passwd.sh
-sudo chmod +x xyz/install.sh
-sudo chmod +x xyz/ngrok.sh
-sudo bash xyz/passwd.sh
-sudo bash xyz/install.sh
-sudo bash xyz/ngrok.sh
